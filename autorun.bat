025b267d086c84115435ff281d31f4d38711e1ad
Geir Landrö <geirlandro@gmail.com>
2012-07-27 14:40:28 +0000

    initial import

---
+++ autorun.bat
@@ -0,0 +1,29 @@
+@ECHO OFF
+
+REM Easier navigation
+doskey ..=cd..
+doskey \=cd \
+
+REM unix style
+doskey ls=dir $*
+doskey ll=dir $*
+doskey cat=type $*
+doskey grep=find "$1" $2
+doskey mv=ren $*
+doskey rm=del $*
+
+REM list only directories
+doskey lsd=dir /A:D $*
+
+REM Programs
+doskey subl="%PROGRAMFILES%\Sublime Text 2\sublime_text.exe" $*
+
+REM Start an HTTP server from a directory and open google chrome
+doskey server="%USERPROFILE%\AppData\Local\Google\Chrome\Application\chrome.exe" http://localhost:8000 $T python -m SimpleHTTPServer
+
+REM Shortcuts
+doskey p=cd \projects
+doskey w=cd \work
+doskey eve=cd \depot\web\main\eveonline\
+doskey dust=cd \depot\web\main\dust514\
+doskey g=git
